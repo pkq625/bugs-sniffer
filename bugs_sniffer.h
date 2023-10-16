ca63e5abf95544e8500cda02a456e2049c127feb
pkq625 <pkq.6255@gmail.com>
2023-10-16 14:40:40 +0800

    test3

--- bugs_sniffer.h
+++ bugs_sniffer.h
@@ -8,19 +8,72 @@
 #include<arpa/inet.h>
 #include<netinet/in.h>
 #include<cerrno>
-// struct
+#include<vector>
+#include<signal.h>
+#include<fcntl.h>
+#include<sys/types.h>
+#include<cstring>
+#include<unistd.h>
+#include<stdlib.h>
+
+// define some common number
+#define ETHER_ADDR_LEN 6
+
+#define ETHER_TYPE_IPV4      (0x0800)
+#define ETHER_TYPE_IPV6      (0x86DD)
+#define ETHER_TYPE_ARP       (0x0806)
+#define ETHER_TYPE_PARP      (0x8035) /* DARP */
+#define ETHER_TYPE_APPLETALK (0x80F3)
+#define ETHER_TYPE_PPP       (0x880B)
+#define ETHER_TYPE_LLDP      (0x88CC)
+#define ETHER_TYPE_VLAN      (0x9100)
+#define ETHER_TYPE_VLAN2      (0x9200)
+
+// IP offset
+#define IP_RF      (0x8000) /* 保留 */
+#define IP_DF      (0x4000) /* 别分段 */
+#define IP_MF      (0x2000) /* 多碎片标志 */
+#define IP_OFFMASK (0x1FFF) /* 分段 */
+
+#define IP_TYPE_ICMP (1)
+#define IP_TYPE_IGMP (2)
+#define IP_TYPE_IP (4)
+#define IP_TYPE_TCP (6)
+#define IP_TYPE_UDP (17)
+#define IP_TYPE_RDP (27)
+#define IP_TYPE_OSPF (89)
+
+// tcp head flags
+#define TH_FIN (0x01)
+#define TH_SYN (0x02)
+#define TH_RST (0x04)
+#define TH_PUSH (0x08)
+#define TH_ACK (0x10)
+#define TH_URG (0x20)
+#define TH_ECE (0x40)
+#define TH_CWR (0x80)
+
+// define header struct
 struct eth_header{
-    u_char dst_mac[6];
-    u_char src_mac[6];
-    u_short eth_type;
+    u_char dst_mac[ETHER_ADDR_LEN]; /* target host mac address */
+    u_char src_mac[ETHER_ADDR_LEN]; /* source host mac address */
+    u_short eth_type;               /* IP: 0x0800, IPv6: 0x86DD, ARP: 0x0806, RARP: 0x8035*/
 };
 
 struct ip_header{
-    int version:4;
-    int header_len:4;
-    u_char tos:8;
-    int total_len:16;
-    int ident:16;
+    /* the byte order matters actually... */
+    #if BYTE_ORDER == LITTLE_ENDIAN
+    u_int header_len: 4;
+    u_int version: 4;
+    #if BYTE_ORDER == BIG_ENDIAN
+    u_int version:4;    /* ip version */
+    u_int header_len:4; /* header len */
+    #endif
+    #endif
+    u_char tos:8;      /* service type */
+    u_short total_len; /* .. */
+    u_short ident;     /* identifier */
+    u_short offset;    /**/
     u_char ttl:8;
     u_char protocol:8;
     int checksum:16;
@@ -37,7 +90,7 @@ struct tcp_header{
     u_char flags;
     u_short wind_size;
     u_short check_sum;
-    u_short urg_ptr;
+    u_short urg_ptr;   /* for urgent... */
 };
 
 struct udp_header{
@@ -51,14 +104,12 @@ ip_header* ip_hdr;
 tcp_header* tcp_hdr;
 udp_header* udp_hdr;
 // callback function
-void pcap_callback(unsigned char* arg, const struct pcap_pkthdr* packet_header, const unsigned char* packet_content);
 int listAll(vector<char*>& result); // 返回所有的dev_name
-void getDevIP();
-void getDev();
-void getDevInfo();
-void getDevStatistics();
+void get_dev_IP(char* dev_name, vector<char*>& results);
+void get_dev_info(char* dev_name vector<char*>& results);
+void get_dev_statistics(char* dev_name, vector<vector<int> >& results);
 
-void analyze();
+void do_analyze(char* dev_name, vector<vector<<char*>>& results);
 void do_ether();
 void do_ip();
 void do_tcp();
@@ -73,4 +124,10 @@ void do_trace();
 
 void do_save();
 void do_load();
+
+void findProcess(char* name);
+
+// callback functions
+void pcap_callback(unsigned char* args, const struct pcap_pkthdr* packet_header, const unsigned char* packet_content);
+int pcap_protocal(pcap_dumper_t* args, const struct pcap_pkthdr* packet_header, const u_char* packet);
 #endif
--- test_func.cpp
+++ test_func.cpp
@@ -33,6 +33,29 @@ int getAddr(char* dev_name, char err_buff, vector<char*>&results){
         return 0;
     }
 }
+int pcap_protocal(pcap_dumper_t* args, const struct pcap_pkthdr* packet_header, const u_char* packet){
+    pcap_dump((char*)args, packet_header, packet);
+    printf("packet size: %u, data len: %u\n", packet_header->len, packet->caplen);
+    struct ether_header* etherhdr = (struct ehter_header*) packet;
+    unsigned char* src_mac = ehterhdr->src_mac;
+    unsigned char* dst_mac = ehterhdr->dst_mac;
+    printf("src mac: %x:%x:%x:%x:%x:%x\n", src_mac[0], src_mac[1], src_mac[2], src_mac[3], src_mac[4], src_mac[5]);
+    printf("src mac: %x:%x:%x:%x:%x:%x\n", dst_mac[0], dst_mac[1], dst_mac[2], dst_mac[3], dst_mac[4], dst_mac[5]);
+    printf("ehter type: %u\n", etherhdr->ether_type);
+
+    int eth_len = sizeof(struct ether_header); 
+    if (ntohs(etherhdr->ether_type) == ETHERTYPE_IPV4){
+        // transfer 16 bit network bytes into host bytes
+        printf("IPv4: ")
+    }else if (ntohs(etherhdr->ether_type) == ETHERTYPE_IPV6){
+        printf("IPv6!");
+    }else{
+        printf("Others");
+    }
+    
+    return 0;
+}
+
 void test_listAll(){
     vector<char*>results;
     int n = listAll(results);
